f8b38ef004c7ee2907536f20c2ac05be9432ea91
Marko <graphneenja@gmail.com>
2026-07-05 08:56:55 -0700

    data transformation cmplt

--- networksecurity/main.py
+++ networksecurity/main.py
@@ -1,9 +1,13 @@
+# These do the physical work i.e. read/write to the filesystem w/its data
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
+
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+# this compiles/collates the config data from various sources into one data struct.
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
-from networksecurity.components.data_validation import DataValidation
 import sys
 
 if __name__=="__main__":
@@ -23,6 +27,12 @@ if __name__=="__main__":
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("Data Validation completed")
         print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(f"Data Transformation: {data_transformation_artifact}")
+        logging.info("Data Transformation completed")
+
     except Exception as e:
         raise NetworkSecurityException(e,sys)
     
